2f147495c46f3b0b71449a955cfd585df2cfea6e
James Hoegerl <james@jamess-mbp.lan>
2024-09-27 01:23:42 -0400

    added function url

--- lib/bauble/resources/ruby_function.rb
+++ lib/bauble/resources/ruby_function.rb
@@ -8,30 +8,53 @@ module Bauble
   module Resources
     # a ruby lambda function
     class RubyFunction < BaseResource
-      attr_accessor :handler, :name, :role
+      attr_accessor :handler, :name, :role, :code_dir, :function_url
 
-      def initialize(app, name:, handler:, role: nil)
+      def initialize(app, name:, handler:, code_dir:, role: nil, function_url: false)
         super(app)
         @name = name
         @handler = handler
+        @code_dir = code_dir
         @role = role
+        @function_url = function_url
       end
 
       def bundle
-        assets_dir = File.join(Dir.pwd, '.bauble', 'assets', @app.bundle_hash)
+        # generate the asset directory path
+        assets_dir = File.join(@app.config.asset_dir, @app.bundle_hash)
+
+        # create the asset directory if it doesn't exist
         FileUtils.mkdir_p(assets_dir)
 
+        # create the zipfile path
         zipfile_name = File.join(assets_dir, "#{name}.zip")
+
+        # remove the zipfile if it already exists
         FileUtils.rm_f(zipfile_name)
 
+        # create the zipfile
         Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-          Dir.glob('app/**/*').each do |file|
-            zipfile.add(file.sub('app/', ''), file)
+          # add the code directory to the zipfile
+          Dir.glob(File.join(@code_dir, '**', '*')).each do |file|
+            zipfile_path = File.join(File.basename(@code_dir), file.sub("#{code_dir}/", ''))
+            zipfile.add(zipfile_path, file)
           end
+
+          # add the Gemfile and Gemfile.lock to the zipfile
+          gemfile_path = File.join(Dir.pwd, 'Gemfile')
+          gemfile_lock_path = File.join(Dir.pwd, 'Gemfile.lock')
+          zipfile.add('Gemfile', gemfile_path) if File.exist?(gemfile_path)
+          zipfile.add('Gemfile.lock', gemfile_lock_path) if File.exist?(gemfile_lock_path)
         end
       end
 
       def synthesize
+        template = base_template
+        template.merge!(function_url_template_addon) if @function_url
+        template
+      end
+
+      def base_template
         {
           @name => {
             'type' => 'aws:lambda:Function',
@@ -43,6 +66,19 @@ module Bauble
                 'fn::fileArchive' => "#{@app.config.asset_dir}/#{@app.bundle_hash}/#{@name}.zip"
               },
               'role' => "${#{@role.role_name}.arn}"
+            }
+          }
+        }
+      end
+
+      def function_url_template_addon
+        {
+          'function_url' => {
+            'type' => 'aws:lambda:FunctionUrl',
+            'name' => "#{@name}Url",
+            'properties' => {
+              'functionName' => "${#{@name}}",
+              'authorizationType' => 'NONE'
             }
           }
         }
