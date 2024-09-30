13b4d9c0ba2cb240800bbe14b85d2d44a24711d8
James Hoegerl <james.hoegerl@gmail.com>
2024-09-30 17:33:05 -0400

    move handler to handlers dir, update docker command

--- demo_app/app/handlers/hello_world.rb
+++ demo_app/app/handlers/hello_world.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
-require 'httparty'
-require_relative 'models/post'
+require_relative '../models/post'
 
 def handler(event:, context:) # rubocop:disable Lint/UnusedMethodArgument
   puts event.to_json
-  HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
+  puts 'HET!!'
   Post.find(id: 'hi').to_h.to_json
 end
