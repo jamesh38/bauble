ec3dfcb0771566297a7da75ebd1b5c8857f81c63
James Hoegerl <james.hoegerl@gmail.com>
2024-09-30 21:00:06 -0400

    added command builder

---
+++ lib/bauble/cli/docker_command_builder.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module Bauble
+  module Cli
+    # Builds a docker command
+    class DockerCommandBuilder
+      def initialize
+        @command = 'docker run '
+      end
+
+      def with_rm
+        @command += '--rm '
+        self
+      end
+
+      def with_volume(volume)
+        @command += "-v #{volume} "
+        self
+      end
+
+      def with_workdir(workdir)
+        @command += "-w #{workdir} "
+        self
+      end
+
+      def with_entrypoint(entrypoint)
+        @command += "--entrypoint #{entrypoint} "
+        self
+      end
+
+      def with_platform(platform)
+        @command += "--platform #{platform} "
+        self
+      end
+
+      def with_image(image)
+        @command += "#{image} "
+        self
+      end
+
+      def with_command(cmd)
+        @command += "-c \"#{cmd}\""
+        self
+      end
+
+      def build
+        @command
+      end
+    end
+  end
+end
