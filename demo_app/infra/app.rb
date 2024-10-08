618195f4b7917999f094cf5e906431ffeb4375e0
James Hoegerl <james.hoegerl@gmail.com>
2024-10-08 14:45:58 -0400

    half sqs

--- demo_app/infra/app.rb
+++ demo_app/infra/app.rb
@@ -25,13 +25,5 @@ fun = Bauble::Resources::RubyFunction.new(
   role: role
 )
 
-rule = Bauble::Resources::EventBridgeRule.new(
-  app,
-  rule_name: 'myrule',
-  event_pattern: {
-    source: ['custom-source'],
-    'detail-type': ['hello-world']
-  }
-)
-
-rule.add_target(fun)
+queue = Bauble::Resources::SQSQueue.new(app, name: 'my-queue')
+queue.add_lambda_target(fun)
