83d2ae7ee511d796934326fc4109d39a93f59664
James Hoegerl <james.hoegerl@gmail.com>
2024-10-08 14:46:47 -0400

    half sqs again

---
+++ lib/bauble/resources/sqs_queue.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require_relative 'resource'
+
+# bauble resources to manage infrastructure
+module Bauble
+  module Resources
+    # SQS Queue
+    class SQSQueue < Resource
+      attr_accessor :name, :visibility_timeout, :lambda_targets
+
+      def initialize(app, name:, visibility_timeout: 30)
+        super(app)
+        @name = name
+        @visibility_timeout = visibility_timeout
+        @lambda_targets = []
+      end
+
+      def synthesize
+        base_template = {
+          @name => {
+            'type' => 'aws:sqs:Queue',
+            'properties' => {
+              'name' => @name,
+              'visibilityTimeoutSeconds' => @visibility_timeout
+            }
+          }
+        }
+
+        @lambda_targets.each { |target| base_template.merge!(target) }
+
+        base_template
+      end
+
+      def bundle
+        true
+      end
+
+      # Function to add a Lambda function as a target to the SQS Queue
+      def add_lambda_target(function)
+        @lambda_targets << {
+          "#{@name}_to_#{function.name}" => {
+            'type' => 'aws:lambda:EventSourceMapping',
+            'properties' => {
+              'eventSourceArn' => "${#{name}.arn}",
+              'functionName' => "${#{function.name}.name}",
+              'batchSize' => 10
+            }
+          }
+        }
+      end
+    end
+  end
+end
