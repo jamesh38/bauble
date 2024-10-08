5a040e6c34321e2ebc3ec11753d7b683e3f4afc2
James Hoegerl <james.hoegerl@gmail.com>
2024-10-08 14:46:47 -0400

    half sqs again

--- lib/bauble.rb
+++ lib/bauble.rb
@@ -7,6 +7,7 @@ require_relative 'bauble/resources/s3_bucket'
 require_relative 'bauble/resources/ruby_function'
 require_relative 'bauble/resources/gem_layer'
 require_relative 'bauble/resources/event_bridge_rule'
+require_relative 'bauble/resources/sqs_queue'
 require_relative 'bauble/stack'
 
 module Bauble
