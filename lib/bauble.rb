8db957c822db62a94846d9853cc188fa9b991608
James Hoegerl <james.hoegerl@gmail.com>
2024-10-08 00:06:41 -0400

    event bridge rule resource

--- lib/bauble.rb
+++ lib/bauble.rb
@@ -6,6 +6,7 @@ require_relative 'bauble/resources/iam_role'
 require_relative 'bauble/resources/s3_bucket'
 require_relative 'bauble/resources/ruby_function'
 require_relative 'bauble/resources/gem_layer'
+require_relative 'bauble/resources/event_bridge_rule'
 require_relative 'bauble/stack'
 
 module Bauble
