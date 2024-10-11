3722105cb4b1024ac4478c9af4e5601fac974fc0
James Hoegerl <james.hoegerl@gmail.com>
2024-10-11 12:30:31 -0400

    make a resource name

--- demo_app/app/handlers/hello_world.rb
+++ demo_app/app/handlers/hello_world.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-require_relative '../models/post'
-
 def handler(event:, context:) # rubocop:disable Lint/UnusedMethodArgument
   puts event.to_json
-  puts 'HET!!'
-  post = Post.find(id: 'hi').to_h
-  { statusCode: 200, body: { post: post }.to_json }
 end
